17ce85831d63a8dea2e09286f7e14b4f9d8d9340
Kurono <chrono.social@outlook.com>
2022-12-21 17:37:45 +0800

    add: idea implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import moment from "moment";
+import {range} from "range";
 
-const birthDate = new Date("July 23, 2001");
+const birthDate = new Date("July 26, 2002");
 const dateToday = new Date();
 
 const birthDateMoment = moment(birthDate);
@@ -12,4 +13,12 @@ const weeksAlive = intervalTime.asWeeks().toFixed(0);
 const minWeeksOfLife = 4500;
 const weeksLeftInYourLife = minWeeksOfLife - weeksAlive;
 
-console.log("%s weeks left", weeksLeftInYourLife);
+const weeksAliveArray = range(1, weeksAlive);
+
+for (let index = 0; index < minWeeksOfLife; index++) {
+    if(weeksAliveArray[index] === undefined) {
+        console.log("0 unfilled", index)
+    } else {
+        console.log("1 filled", weeksAliveArray[index])
+    }
+}
